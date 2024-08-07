17fd04a900ecc77a73bb5402c20573d52d188a51
Matteo <mkinkela123@gmail.com>
2024-08-07 19:28:46 +0200

    feat: init app

---
+++ src/main/kotlin/com/calculate/routing/RoutingApplication.kt
@@ -0,0 +1,11 @@
+package com.calculate.routing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoutingApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoutingApplication>(*args)
+}
